4d13c082fe89e70e0940aa841229d11d266c6a10
xsrf <git@xsrf.de>
2021-04-11 16:24:49 +0200

    do not self execute example lib

--- extension/lib.js
+++ extension/lib.js
@@ -4,6 +4,3 @@ myLibraryFunction = () => {
 var myLibraryFunctionVar = () => {
     console.log('myLibraryFunctionVar');
 }
-console.log('lib.js loaded');
-myLibraryFunction();
-myLibraryFunctionVar();
